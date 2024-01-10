4b2fba8d082ef3fb8f82d5d6250fb4dcf6f6df49
Masood <masoodit3036@gmail.com>
2024-01-10 15:23:21 +0530

    clean

--- cypress-test/cypress/e2e/integration-test/assertionChallenge.cy.js
+++
@@ -1,6 +0,0 @@
-///<reference types="cypress"/>
-describe("Test", () => {
-  it("Launch browser", () => {
-    cy.visit("http://www.google.com");
-  });
-});
--- cypress-test/cypress/e2e/integration-test/firstCypressTest.cy.js
+++
@@ -1,5 +0,0 @@
-describe("Test", () => {
-  it("Launch browser", () => {
-    cy.visit("http://www.google.com/");
-  });
-});
